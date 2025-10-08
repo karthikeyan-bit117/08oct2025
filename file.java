f897716a741e5d5cb27c9c66a6acab653018be8d
root <root@ip-172-31-2-200.ec2.internal>
2025-10-08 11:08:20 +0000

    This is my second commit

--- file.java
+++ file.java
@@ -1,3 +1,4 @@
+This is my second commit
 public class SpiralPatternExample2 {
     // Function to print the spiral pattern
     public static void printPattern(int n) {
@@ -19,6 +20,35 @@ public class SpiralPatternExample2 {
         System.out.print("Enter the value of n: ");
         int n = sc.nextInt();
         System.out.println();
+        printPattern(n);
+    }
+}
+public class SpiralPatternExample2 {
+
+    // Function to print the spiral pattern
+    public static void printPattern(int n) {
+        // Determines the boundary size of the array
+        int size = 2 * n - 1;
+
+        // Inner loop
+        for (int i = 1; i <= size; i++) {
+            // Outer loop
+            for (int j = 1; j <= size; j++) {
+                // Calculates and prints the values for pattern
+                System.out.print(Math.max(Math.abs(i - n), Math.abs(j - n)) + 1 + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Driver code
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+        System.out.println();
+
+        // Function calling
         printPattern(n);
     }
 }
